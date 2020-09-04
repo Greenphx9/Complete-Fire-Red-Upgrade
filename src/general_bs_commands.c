0175d6235d0de81756327b4b6df9c221817f62af
Skeli <sesyl.schweitzer@gmail.com>
2020-09-04 16:38:07 +0300

    Fix Nature Power Picking a Random Target in Doubles

--- src/general_bs_commands.c
+++ src/general_bs_commands.c
@@ -4420,10 +4420,9 @@ void atkCA_setforcedtarget(void) //Follow me
 	gBattlescriptCurrInstr++;
 }
 
-void atkCC_callterrainattack(void) { //nature power
+void atkCC_callterrainattack(void) //nature power
+{
 	gCurrentMove = GetNaturePowerMove();
-
-	gBankTarget = GetMoveTarget(gCurrentMove, 0);
 	TryUpdateCalledMoveWithZMove();
 	UpdateMoveStartValuesForCalledMove();
 	BattleScriptPush(gBattleScriptsForMoveEffects[gBattleMoves[gCurrentMove].effect]);
