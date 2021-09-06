6617ea7bde5ee8670f5bf9b7e4a1c856025dd5a0
Skeli <sesyl.schweitzer@gmail.com>
2021-09-06 11:02:18 -0400

    Fix Ghost-types Switching out of Sky Drop

--- src/Battle_AI/ai_util.c
+++ src/Battle_AI/ai_util.c
@@ -2300,6 +2300,9 @@ s32 GetMonPassiveRecovery(struct Pokemon* mon, unusedArg u8 bank)
 
 bool8 IsTrapped(u8 bank, bool8 switching)
 {
+	if (gStatuses3[bank] & (STATUS3_SKY_DROP_ATTACKER | STATUS3_SKY_DROP_TARGET))
+		return TRUE; //Can never switch out when in the air
+
 	if (!CanBeTrapped(bank)
 	|| (!switching && ABILITY(bank) == ABILITY_RUNAWAY)
 	|| (!switching && ITEM_EFFECT(bank) == ITEM_EFFECT_CAN_ALWAYS_RUN))
@@ -2310,7 +2313,7 @@ bool8 IsTrapped(u8 bank, bool8 switching)
 		|| (ABILITY_ON_OPPOSING_FIELD(bank, ABILITY_SHADOWTAG) && IsTrappedByAbility(bank, ABILITY_SHADOWTAG))
 		|| (ABILITY_ON_OPPOSING_FIELD(bank, ABILITY_ARENATRAP) && IsTrappedByAbility(bank, ABILITY_ARENATRAP))
 		|| (ABILITY_ON_OPPOSING_FIELD(bank, ABILITY_MAGNETPULL) && IsTrappedByAbility(bank, ABILITY_MAGNETPULL))
-		|| gStatuses3[bank] & (STATUS3_ROOTED | STATUS3_SKY_DROP_TARGET)
+		|| (gStatuses3[bank] & STATUS3_SKY_DROP_TARGET)
 		|| IsFairyLockActive())
 			return TRUE;
 	}
