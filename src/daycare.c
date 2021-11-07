2d19d2760a9fc354dadb52f09189f9eca561f4ad
Skeli <sesyl.schweitzer@gmail.com>
2021-11-07 17:49:58 +0200

    Optional Skip Egg Nickname Offer

--- src/daycare.c
+++ src/daycare.c
@@ -1015,3 +1015,12 @@ u8 GetLevelAfterDaycareSteps(struct BoxPokemon* mon, u32 steps)
 	SetBoxMonData(&tempMon, MON_DATA_EXP, &experience);
 	return GetLevelFromBoxMonExp(&tempMon);
 }
+
+bool8 ShouldSkipOfferEggHatchNickname(void)
+{
+	#ifdef FLAG_DONT_OFFER_NICKNAMES_BATTLE
+	return FlagGet(FLAG_DONT_OFFER_NICKNAMES_BATTLE);
+	#else
+	return FALSE;
+	#endif
+}
